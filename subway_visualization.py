9c8d5ac02f9ea402e29af19c9fb6dab0da8de08f
unknown <bear0439@gmail.com>
2021-04-01 22:25:10 +0900

    Update subway_visualization.py

--- subway_visualization.py
+++ subway_visualization.py
@@ -94,3 +94,65 @@ plt.xlabel('시간대')
 plt.ylabel('승객 수')
 plt.legend()
 plt.show()
+
+
+pip install fbprophet
+pip install plotly          # 프로펫 라이브러리 설치
+
+
+fb_subway = subway.reset_index()  #일자를 datetime으로 변경하기 위해 인덱스 해제
+
+fb_subway['일자']=pd.to_datetime(fb_subway['일자']) 
+
+fb_subway.info()
+
+fb_subway
+
+fb_ride_subway=fb_subway.loc[fb_subway['구분']=='승차']
+
+fb_takeoff_subway=fb_subway.loc[fb_subway['구분']=='하차']
+
+ride = fb_ride_subway.iloc[:,[0,2]].set_index('일자')       # 데이터 프레임의 자료를 더하기 위해 인덱스 설정
+
+takeoff = fb_takeoff_subway.iloc[:,[0,2]].set_index('일자')
+
+total = ride +takeoff
+
+total=total.reset_index()
+
+
+total
+
+
+
+from fbprophet import Prophet       #프로펫 라이브러리 import
+
+total = total.rename(columns={'일자':'ds', '계':'y'})  #프로펫 사용을 위한 컬럼 명 변경
+
+total
+
+total.info()
+
+total.isna().sum()      #데이터 다시 한번 확인
+
+
+
+m = Prophet()
+
+m.fit(total)            #프로펫 학습시키기
+
+
+future=m.make_future_dataframe(periods=365) # 365일치를 예측하시오.
+
+forecast=m.predict(future)
+#예측이 필요하면 m.make_future_dataframe(periods=365)를 이용해서 빈 일자를 만들고 predict을 사용
+
+forecast
+#yhat = y의 예측값
+
+# 차트로 확인하시오.
+m.plot(forecast, xlabel= 'Date', ylabel='passengers')
+plt.show()
+
+m.plot_components(forecast)
+plt.show()
